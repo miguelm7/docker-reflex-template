7dfa0888f8a5d6a67113ef02b89b47bf163bd34d
miguelm7 <miguelmore100@gmail.com>
2024-05-02 18:17:20 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This is a simple script to build the docker image
+echo "Pulling changes..."
+
+git pull
+
+echo "Building docker image..."
+
+docker compose build
+
+echo "Bringing up containers..."
+
+docker compose up -d
+
+echo "Deployment finished succesfully!"
