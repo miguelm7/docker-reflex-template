fdfc79663dced8bada5c975c81a01df038f20a89
miguelm7 <miguelmore100@gmail.com>
2024-05-02 18:47:14 +0200

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 #This is a simple script to build the docker image
+
+if [ $# -eq 0 ]
+  then
+    echo "No domain specified, will default to localhost, no SSL"
+    read -p "Are you sure? [y/n] " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+        then
+            echo "Deploy aborted"
+            [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    fi
+fi
+
+
+echo "Initiating deploy for $1"
+
 echo "Pulling changes..."
 
 git pull
 
 echo "Building docker image..."
 
-docker compose build
+DOMAIN=$1 docker compose build
 
 echo "Bringing up containers..."
 
-docker compose up -d
+DOMAIN=$1 docker compose up -d
 
 echo "Deployment finished succesfully!"
