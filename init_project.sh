590f3c3c8ae29c757d897213a72a2b63277da579
miguelm7 <miguelmore100@gmail.com>
2024-05-04 10:59:53 +0200

    add init project inicialization script

---
+++ init_project.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#This is a simple script to build the docker image
+
+if [ $# -eq 0 ]
+  then
+    echo "No project name specified, please choose one"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+
+echo "Initializing Reflex project..."
+
+cd ..
+
+mkdir $1
+
+cd $1
+
+cp -r ../docker-reflex-template/docker_files/. .
+cp ../docker-reflex-template/deploy.sh .
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip install reflex --upgrade 
+
+pip freeze > requirements.txt
+
+reflex init
+
+echo "Reflex project initialized succesfully!"
